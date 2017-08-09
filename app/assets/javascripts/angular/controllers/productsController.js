e40511529749cf8d7ecab358007abe2c0d9da04d
Shelby Solomon <shelby@shelbysolomon.net>
2017-08-09 17:26:16 -0500

    purchase form front-end done

--- app/assets/javascripts/angular/controllers/productsController.js
+++ app/assets/javascripts/angular/controllers/productsController.js
@@ -1,5 +1,13 @@
 demoApp.controller('productsController', ['$scope', '$log', 'Product', function($scope, $log, Product) {
+  $scope.purchaseButtonText = "Purchase!";
+  $scope.submitted = false;
+
   Product.query(function(products){
     $scope.products = products;
   });
+
+  $scope.purchase = function(products){
+    $scope.purchaseButtonText = "Processing...";
+    $scope.submitted = true;
+  };
 }]);
