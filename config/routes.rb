a3efad28017d8cbc2184e8f498bb9905cfb341a5
Shelby Solomon <shelby@shelbysolomon.net>
2017-08-09 14:25:24 -0500

    user auth paths created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "products#index"
+
+  devise_scope :user do
+      resources :users
+      get "signup", to: "devise/registrations#new", as: :sign_up
+      get "signin", to: "devise/sessions#new", as: :sign_in
+      delete "signout", to: "devise/sessions#destroy", as: :sign_out
+      put "update", to: "users#update", as: :update
+    end
 end
