871fc78d038824a9ca13f8302022c05028606976
Shelby Solomon <shelby@shelbysolomon.net>
2017-08-09 20:24:58 -0500

    added confirmation/error messages

--- app/assets/javascripts/angular/controllers/productsController.js
+++ app/assets/javascripts/angular/controllers/productsController.js
@@ -1,6 +1,10 @@
 demoApp.controller('productsController', ['$scope', '$log', '$filter', 'Product', 'Purchase', function($scope, $log, $filter, Product, Purchase) {
-  $scope.purchaseButtonText = "Purchase!";
-  $scope.submitted = false;
+  // reset purchase button on page load
+  function resetButtons(){
+    $scope.purchaseButtonText = "Purchase!";
+    $scope.submitted = false;
+  };
+  resetButtons();
 
   // get all products
   Product.query(function(products){
@@ -16,6 +20,11 @@ demoApp.controller('productsController', ['$scope', '$log', '$filter', 'Product'
 
     Purchase.save({
       products: items
+    }, function(response){
+      $('.purchase_section').html('<p>Thanks for your purchase!</p>');
+    }, function(err){
+      $scope.err = "There's been an error"
+      resetButtons();
     });
   };
 }]);
